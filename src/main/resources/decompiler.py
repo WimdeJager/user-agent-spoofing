fecab247977dfea28d83da1e94f748b75ef0fc12
WimdeJager <wimdejager72@gmail.com>
2020-05-27 16:46:01 +0200

    changed from argv to function

--- src/main/resources/decompiler.py
+++ src/main/resources/decompiler.py
@@ -8,13 +8,14 @@ I used this script to learn some androguard skillz.
 by k3170makan
 """
 
-from sys import argv
+# decompile_apk("D:/wimde/cs/bscproject/dataset/Benign-sample-187/a.a.hikidashi.apk", "D:/wimde/cs/bscproject/dataset/Benign-sample-187/output")
+
 from sys import exit
 from sys import path
 
 import os
 
-print path
+print(path)
 
 from androguard.core.bytecodes import apk
 from androguard.core.bytecodes import dvm
@@ -26,37 +27,24 @@ def convert_descriptor(name):
     name = name[1:]
     return name.replace("/",".").replace(";","")
 
-def convert_field_desc(name,field_desc):
-    return
-
-def check_dirs(directory):
-    if not os.path.exists(PREFIX + "/" + directory):
+def check_dirs(directory, prefix):
+    if not os.path.exists(prefix + "/" + directory):
         try:
-            os.makedirs(PREFIX + "/" + directory)
+            os.makedirs(prefix + "/" + directory)
         except:
             pass
 
-def check_path(class_path):
+def check_path(class_path, prefix):
     org_path = class_path.replace(".","/")
     paths = org_path.split("/")
     paths = paths[:len(paths)-1]
 
     for index,folder in enumerate(paths):
-        check_dirs('/'.join(paths[:index+1]))
+        check_dirs('/'.join(paths[:index+1]), prefix)
 
-    return PREFIX + "/" + org_path + ".java"
+    return prefix + "/" + org_path + ".java"
 
-if __name__=="__main__":
-    if len(argv) != 3:
-        print("Usage: ./decompiler [APK] [OUTPUT_DIRECTORY]")
-        print("*please ensure you use the full path for OUTPUT_DIRECTORY...")
-        exit(1)
-
-    apk_path = argv[1]
-    out_path = argv[2]
-
-    PREFIX = out_path
-
+def decompile_apk(apk_path, out_path):
     if not os.path.exists(apk_path):
         print("APK file '%s' not found..." % apk_path)
         exit(1)
@@ -71,7 +59,7 @@ if __name__=="__main__":
 
     for _class in d.get_classes():
         class_path = convert_descriptor(_class.get_name())
-        path = check_path(class_path)
+        path = check_path(class_path, out_path)
         print("[*] writing ...'", path, "'")
 
         if not os.path.exists(path):
