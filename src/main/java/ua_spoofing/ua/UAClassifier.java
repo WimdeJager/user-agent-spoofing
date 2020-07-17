67ab8b4d5e8775ed8ec8e840de82a75548c4e177
WimdeJager <wimdejager72@gmail.com>
2020-07-17 12:27:17 +0200

    create UserAgent class (renamed UAClassifier)

--- src/main/java/ua_spoofing/ua/UAClassifier.java
+++
@@ -1,7 +0,0 @@
-package ua_spoofing.ua;
-
-/**
- * Created by wimde on 11-6-2020.
- */
-public class UAClassifier {
-}
---
+++ src/main/java/ua_spoofing/ua/UserAgent.java
@@ -0,0 +1,20 @@
+package ua_spoofing.ua;
+
+import ua_spoofing.OutputHandler;
+
+/**
+ * Created by wimde on 11-6-2020.
+ */
+public class UserAgent {
+
+  String ua;
+
+  public UserAgent(String ua) {
+    this.ua = ua;
+  }
+
+  public void classify() {
+    
+  }
+
+}
